07d2987c6fc66ff69ace01150a73c141910d35f5
Vlad Bazhanov <bazhanvlad58@yandex.ru>
2018-03-16 22:09:10 +0400

    Fix adaptive

--- js/common.js
+++ js/common.js
@@ -1,6 +1,6 @@
 var menuElem = document.getElementById('burger-menu');
-var buttonElem = menuElem.querySelector('.hamburger');
+var titleElem = menuElem.querySelector('.hamburger');
 
-buttonElem.onclick = function() {
+titleElem.onclick = function () {
     menuElem.classList.toggle('open');
 };
--- src/js/hamburger-menu.js
+++
@@ -1,6 +0,0 @@
-var menuElem = document.getElementById('burger-menu');
-var buttonElem = menuElem.querySelector('.hamburger');
-
-buttonElem.onclick = function() {
-    menuElem.classList.toggle('open');
-};
